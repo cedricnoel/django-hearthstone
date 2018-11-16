5902765ca8a7496abce614967016a54d57ced5b3
Thomas <rachavong.thomas@gmail.com>
2018-11-16 09:45:12 +0100

    add packer folder

--- packet/views.py
+++ packet/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from cards.models import Card
 from .models import Packet
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from random import randint
 
 # Create your views here.
 
 @login_required
 def open_packet(self, packet_id):
+    #TODO: Check if user is connected and if he has enough points
     packet =  get_object_or_404(Packet, pk=packet_id)
-    cards = Card.objects.order_by('-pub_date')[:packet.card_number]
+    count = Card.objects.count()
+    all_cards = Card.objects.all()
+    cards = []
+    for k in range(0, packet.card_number):
+        cards.append(all_cards[randint(0, count - 1)])
     return HttpResponse(cards)
+
 
 @login_required
 def index(request):
