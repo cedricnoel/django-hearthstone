ec3c75ae083c5db5ac889a8416ec94c9101c2c1c
Cédric Noël <cedric.noel.paris@gmail.com>
2019-01-15 21:48:02 +0100

    Update challenge model

--- challenge/models.py
+++ challenge/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Challenge(models.Model):
-    player1 = models.ForeignKey(User, on_delete=models.CASCADE)
-    deck1   = models.ForeignKey(Deck)
-    player2 = models.ForeignKey(User, on_delete=models.CASCADE)
-    deck2   = models.ForeignKey(Deck, blank=True)
-    status  = models.CharField(choices=['pending', 'accepted', 'declined'], default='pending')
-    winner  = models.ForeignKey(User, blank=True)
+    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='challenger')
+    deck1   = models.ForeignKey(Deck, on_delete=models.DO_NOTHING, related_name='challenger_deck')
+    player2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='challenged')
+    deck2   = models.ForeignKey(Deck, blank=True, on_delete=models.DO_NOTHING, related_name='challenged_deck', null=True)
+    status  = models.CharField(max_length=200, default='pending')
+    winner  = models.ForeignKey(User, blank=True, on_delete=models.DO_NOTHING, related_name='wins', null=True)
