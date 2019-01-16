093a343027001c91d997b5cc494b7fa4a72b1832
Unknown <rachavong.thomas@gmail.com>
2019-01-16 22:16:27 +0100

    manage card quantity with deck

--- decks/models.py
+++ decks/models.py
@@ -6,8 +6,13 @@ from django_currentuser.db.models import CurrentUserField
 class Deck(models.Model):
     owner = CurrentUserField()
     name = models.CharField(default = 'My Deck', max_length = 255)
-    items = models.ManyToManyField('cards.Card', related_name="decks", blank=True)
     pub_date = models.DateTimeField(default = datetime.datetime.now(), blank = True)
-
     def __str__(self):
         return self.name
+
+class Deck_cards(models.Model):
+    card = models.ForeignKey("cards.Card", on_delete=models.CASCADE)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    def __str__(self):
+        return self.deck.name + "-" + self.card.name
