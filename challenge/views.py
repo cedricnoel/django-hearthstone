a800b4681dfaa586893e5d9acf230dce530d8362
Cédric Noël <cedric.noel.paris@gmail.com>
2019-01-17 22:17:14 +0100

    Correct fight system

--- challenge/views.py
+++ challenge/views.py
@@ -1,7 +1,7 @@
 import random
 
 from challenge.models import Challenge
-from decks.models import Deck
+from decks.models import Deck, Deck_cards
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -50,31 +50,33 @@ def fight(request, challenge_id):
         challenge = Challenge.objects.get(pk=challenge_id)
 
         game = {
-            'deck1': sorted(challenge.deck1.items.all(), key=lambda y: random.random()),
-            'deck2': sorted(Deck.objects.get(pk=request.POST['deck']).items.all(), key=lambda y: random.random()),
+            #'deck1': sorted(challenge.deck1.items.all(), key=lambda y: random.random()),
+            'newDeck1': sorted(Deck_cards.objects.filter(deck=challenge.deck1), key=lambda y: random.random()),
+            #'deck2': sorted(Deck.objects.get(pk=request.POST['deck']).items.all(), key=lambda y: random.random()),
+            'newDeck2': sorted(Deck_cards.objects.filter(deck=request.POST['deck']), key=lambda y: random.random()),
             'lp1': 100,
             'lp2': 100,
             'winner': ''
         }
 
         for x in range(0, 10):
-            if x % 2 == 0 and x < len(game['deck1']) and x < len(game['deck2']):
-                result = do_battle(game['deck1'][x], game['deck2'][x])
+            if x % 2 == 0 and x < len(game['newDeck1']) and x < len(game['newDeck2']):
+                result = do_battle(game['newDeck1'][x].card, game['newDeck1'][x].card)
                 # You win
                 if result == 1:
-                    game['lp2'] -= game['deck1'][x].atk - game['deck2'][x].atk
+                    game['lp2'] -= game['newDeck1'][x].card.atk - game['newDeck2'][x].card.atk
                 # You lose
                 if result == 2:
-                    game['lp1'] -= game['deck2'][x].atk - game['deck1'][x].atk
+                    game['lp1'] -= game['newDeck2'][x].card.atk - game['newDeck1'][x].card.atk
 
-            if x % 2 > 0 and x < len(game['deck1']) and x < len(game['deck2']):
-                result = do_battle(game['deck2'][x], game['deck1'][x])
+            if x % 2 > 0 and x < len(game['newDeck1']) and x < len(game['newDeck2']):
+                result = do_battle(game['newDeck2'][x].card, game['newDeck1'][x].card)
                 # He win
                 if result == 1:
-                    game['lp1'] -= game['deck2'][x].atk - game['deck1'][x].atk
+                    game['lp1'] -= game['newDeck2'][x].card.atk - game['newDeck1'][x].card.atk
                 # He lose
                 if result == 2:
-                    game['lp2'] -= game['deck1'][x].atk - game['deck2'][x].atk
+                    game['lp2'] -= game['newDeck1'][x].card.atk - game['newDeck2'][x].card.atk
 
         if game['lp1'] > game['lp2']:
             game['winner'] = challenge.player1.username
